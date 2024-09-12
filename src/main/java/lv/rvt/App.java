bc91830f4c352f34f5c9c3c09fc0fb73872edb01
23DP2ASalk <167065543+23DP2ASalk@users.noreply.github.com>
2024-09-12 09:33:13 +0000

    simple calculaor

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,17 +1,20 @@
 package lv.rvt;
 import java.util.Scanner;
+
+import javax.sound.midi.Soundbank;
 public class App 
 {
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ievadi savu vārdu:");
-        String name = scanner.nextLine();
-        System.out.println("Ievadi savu uzvārdu");
-        String surname = scanner.nextLine();
-        System.out.println("Ievadi savu grupu");
-        String group = scanner.nextLine();
-        System.out.println("Students " + name + " " + surname + ", grupa: " + group);
-    }
+        System.out.println("1. skaitlis");
+        int pirmais = Integer.valueOf(scanner.nextLine());
+        System.out.println("2. skaitlis");
+        int otrais = Integer.valueOf(scanner.nextLine());
+        System.out.println(pirmais + "+" + otrais + "=" + (pirmais + otrais));
+        System.out.println(pirmais + "-" + otrais + "=" + (pirmais - otrais));
+        System.out.println(pirmais + "*" + otrais + "=" + (pirmais * otrais));
+        System.out.println(pirmais + "/" + otrais + "=" + ((pirmais * 1.0) / otrais));
 
+    }
 }
