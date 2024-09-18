37e35e1e0ddfc7bcc3859e24bc766603f2675dcb
23DP2ASalk <167065543+23DP2ASalk@users.noreply.github.com>
2024-09-18 10:28:14 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,14 +7,25 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("1. skaitlis");
-        int pirmais = Integer.valueOf(scanner.nextLine());
-        System.out.println("2. skaitlis");
-        int otrais = Integer.valueOf(scanner.nextLine());
-        System.out.println(pirmais + "+" + otrais + "=" + (pirmais + otrais));
-        System.out.println(pirmais + "-" + otrais + "=" + (pirmais - otrais));
-        System.out.println(pirmais + "*" + otrais + "=" + (pirmais * otrais));
-        System.out.println(pirmais + "/" + otrais + "=" + ((pirmais * 1.0) / otrais));
-
+        
+        System.out.println("Your grade:");
+        int grade = Integer.valueOf(scanner.nextLine());
+        if (grade > 100){
+            System.out.println("Grade: Incredible!");
+        }else if(grade >= 90){
+            System.out.println("Grade: 5");
+        }else if(grade >= 80){
+            System.out.println("Grade: 4");
+        }else if(grade >= 70){
+            System.out.println("Grade: 3");
+        }else if(grade >= 60){
+            System.out.println("Grade: 2");
+        }else if(grade >= 50){
+            System.out.println("Grade: 1");
+        }else if(grade >= 0){
+            System.out.println("Grade: Failed");
+        }else if(grade < 0){
+            System.out.println("Grade: Impossible!");
+        }
     }
 }
