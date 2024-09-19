a957f56fa1ac30af54afe2322444c0efd74d8cba
23DP2ASalk <167065543+23DP2ASalk@users.noreply.github.com>
2024-09-19 10:25:57 +0000

    Only positives

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,18 +7,18 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        double value = Double.valueOf(scanner.nextLine());
         
-        if (value >= 5000 && value < 25000) {
-            System.out.println(100 + (value - 5000) * 0.08);
-        } else if (value >= 25000 && value < 55000) {
-            System.out.println(1700 + (value-25000) * 0.1);
-        } else if (value >= 55000 && value < 200000) {
-            System.out.println(4700 + (value-55000) * 0.12);
-        } else if (value >= 200000 && value < 1000000) {
-            System.out.println(22100 + (value-200000) * 0.15);
-        } else if (value >= 1000000) {
-            System.out.println(142100 + (value-1000000) * 0.17);
+        while (true){
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == 0){
+            break;
+            } else if(number > 0) {
+                System.out.println(number * number);
+                continue;
+            } else {
+                System.out.println("Unsuitable number!");
+                continue;
+            }
         }
     }
 }
