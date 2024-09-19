d357ee605c14c7a6280b647d444c16919a329225
23DP2ASalk <167065543+23DP2ASalk@users.noreply.github.com>
2024-09-19 08:32:25 +0000

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,25 +7,18 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
+        double value = Double.valueOf(scanner.nextLine());
         
-        System.out.println("Your grade:");
-        int grade = Integer.valueOf(scanner.nextLine());
-        if (grade > 100){
-            System.out.println("Grade: Incredible!");
-        }else if(grade >= 90){
-            System.out.println("Grade: 5");
-        }else if(grade >= 80){
-            System.out.println("Grade: 4");
-        }else if(grade >= 70){
-            System.out.println("Grade: 3");
-        }else if(grade >= 60){
-            System.out.println("Grade: 2");
-        }else if(grade >= 50){
-            System.out.println("Grade: 1");
-        }else if(grade >= 0){
-            System.out.println("Grade: Failed");
-        }else if(grade < 0){
-            System.out.println("Grade: Impossible!");
+        if (value >= 5000 && value < 25000) {
+            System.out.println(100 + (value - 5000) * 0.08);
+        } else if (value >= 25000 && value < 55000) {
+            System.out.println(1700 + (value-25000) * 0.1);
+        } else if (value >= 55000 && value < 200000) {
+            System.out.println(4700 + (value-55000) * 0.12);
+        } else if (value >= 200000 && value < 1000000) {
+            System.out.println(22100 + (value-200000) * 0.15);
+        } else if (value >= 1000000) {
+            System.out.println(142100 + (value-1000000) * 0.17);
         }
     }
 }
