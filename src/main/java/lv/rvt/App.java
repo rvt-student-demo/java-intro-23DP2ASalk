a6d33390fd86518e15c2b70788c8084677615ede
23DP2ASalk <167065543+23DP2ASalk@users.noreply.github.com>
2024-09-25 10:14:53 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,18 +7,19 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        
+        int sum = 0;
+        int count= 0;
         while (true){
             int number = Integer.valueOf(scanner.nextLine());
             if (number == 0){
             break;
-            } else if(number > 0) {
-                System.out.println(number * number);
-                continue;
+            
             } else {
-                System.out.println("Unsuitable number!");
-                continue;
+                sum += number;
+                count++;
             }
         }
+        System.out.println("Numbers :" + count);
+        System.out.println("Sum: " + sum);
     }
 }
