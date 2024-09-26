0e40d811a2efc297d0dd48567d991242dd5e5597
23DP2ASalk <167065543+23DP2ASalk@users.noreply.github.com>
2024-09-26 09:11:53 +0000

    Counting to hundred

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,19 +7,11 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        int sum = 0;
-        int count= 0;
-        while (true){
-            int number = Integer.valueOf(scanner.nextLine());
-            if (number == 0){
-            break;
-            
-            } else {
-                sum += number;
-                count++;
-            }
+        System.out.println("Ievadiet skaitli: ");
+        int start = Integer.valueOf(scanner.nextLine());
+        int end = 101;
+        for (int i = start; i < end; i++) {
+            System.out.println(i);
         }
-        System.out.println("Numbers :" + count);
-        System.out.println("Sum: " + sum);
     }
 }
