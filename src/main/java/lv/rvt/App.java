4c890c01c07924f47f5a0a6b456fce90d8fac646
23DP2ASalk <167065543+23DP2ASalk@users.noreply.github.com>
2024-10-02 10:56:02 +0000

    From one to parameter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,38 +7,15 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        int sum = 0;
-        int numbers = 0;
-        double avg = 0;
-        int even = 0;
-        int odd = 0;
-
-        while(true){
-            System.out.println("Ievadiet skaitli(lsi beigtu -1): ");
-            int num = Integer.valueOf(scanner.nextLine());
-
-            if (num == -1) {
-                System.out.println("Thx! Bye!");
-                if (numbers > 0) {
-                    avg = (double) sum / numbers;
-                }
-                System.out.println("Sum: " + sum);
-                System.out.println("Numbers: " + numbers);
-                System.out.println("Average: " + avg);
-                System.out.println("Even: " + even);
-                System.out.println("Odd: " + odd);
-                break;
-            } 
-            else{
-                sum += num;
-                numbers++;
-                if (num % 2 == 0){
-                    even++;
-                } else {
-                    odd++;
-                }
-            }
+        System.out.println("Give a number");
+        int num = Integer.valueOf(scanner.nextLine());
+        printUntilNumber(num);
         }
-        scanner.close();
+    public static void printUntilNumber(int numOfTimes) {
+        int i = 1;
+        while (i <= numOfTimes) {
+            System.out.println(i++);
+        }
+        
     }
 }
