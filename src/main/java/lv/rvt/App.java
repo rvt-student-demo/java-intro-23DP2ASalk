e7639269d4d39ed98bb013566e19151a4fd9c942
23DP2ASalk <167065543+23DP2ASalk@users.noreply.github.com>
2024-10-30 11:24:30 +0000

    Decreasing counter (3 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,27 +1,17 @@
 package lv.rvt;
 import java.util.*;
 public class App {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> numbers = new ArrayList<>();
-        while (true) {
-            int input = scanner.nextInt();
-            if (input == 0) {
-                break;
-            }
-            numbers.add(input);
+    public class MainProgram {
+        public static void main(String[] args) {
+            DecreasingCounter counter = new DecreasingCounter(100);
+    
+            counter.printValue();
+    
+            counter.reset();
+            counter.printValue();
+    
+            counter.decrement();
+            counter.printValue();
         }
-        if (numbers.size() >= 3) {
-            int sumSecondThird = numbers.get(1) + numbers.get(2);
-            System.out.println("Sum of second and third numbers: " + sumSecondThird);
-        } else {
-            System.out.println("Not enough numbers entered.");
-        }
-        int totalSum = 0;
-        for (int number : numbers) {
-            totalSum += number;
-        }
-        System.out.println("Array: " + numbers);
-        System.out.println("Sum of all ArrayList elements: " + totalSum);
     }
 }
