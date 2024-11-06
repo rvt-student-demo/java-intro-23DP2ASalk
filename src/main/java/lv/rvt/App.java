5f9028a7e7688ef229847fd7af4a8f1bb3c7911b
23DP2ASalk <167065543+23DP2ASalk@users.noreply.github.com>
2024-11-06 11:26:03 +0000

    Agent

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,15 +3,11 @@ import java.util.*;
 public class App {
     public class MainProgram {
         public static void main(String[] args) {
-            DecreasingCounter counter = new DecreasingCounter(100);
-    
-            counter.printValue();
-    
-            counter.reset();
-            counter.printValue();
-    
-            counter.decrement();
-            counter.printValue();
+            Agent bond = new Agent("James", "Bond");
+            bond.toString();
+            System.out.println(bond);
+            Agent ionic = new Agent("Ionic", "Bond");
+            System.out.println(ionic);
         }
     }
 }
