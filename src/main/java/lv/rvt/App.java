baa873bacd15ec7e74f0b3264cb805a8207c6a53
23DP2ASalk <167065543+23DP2ASalk@users.noreply.github.com>
2024-11-07 08:59:54 +0000

    Payment Card (6 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,32 +3,25 @@ import java.util.*;
 public class App {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
- 
-        Statistics statistics = new Statistics();
-        Statistics even = new Statistics();
-        Statistics odd = new Statistics();
-        {
-            System.out.println("Enter numbers: ");
-            while (true) {
- 
-                int input = Integer.valueOf(scanner.nextLine());
-                if (input == -1) {
-                    break;
-                }
-                if (input % 2 == 0) {
-                    even.addNumber(input);
-                } else {
-                    odd.addNumber(input);
- 
-                }
- 
-                statistics.addNumber(input);
- 
-            }
-            System.out.println("Sum: " + statistics.sum());
-            System.out.println("Sum of even numbers: " + even.sum());
-            System.out.println("Sum of odd numbers: " + odd.sum());
+        PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
+            
+        paulsCard.eatHeartily();
+        System.out.println("Paul: " + paulsCard);
+        mattsCard.eatAffordably();
+        System.out.println("Matt: " + mattsCard);
+        
+        paulsCard.addMoney(20);
+        System.out.println("Paul: " + paulsCard);
+        
+        mattsCard.eatHeartily();
+        System.out.println("Matt: " + mattsCard);
+        
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+        System.out.println("Paul: " + paulsCard);
+        
+        mattsCard.addMoney(50);
+        System.out.println("Matt: " + mattsCard);   
     }
-}
 }
