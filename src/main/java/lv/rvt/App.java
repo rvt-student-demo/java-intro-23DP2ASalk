3e1d1075324581c467598dc4201100a0d89122ae
23DP2ASalk <167065543+23DP2ASalk@users.noreply.github.com>
2024-11-28 09:19:04 +0000

    Product

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,18 +1,17 @@
 package lv.rvt;
 import java.util.*;
 import java.io.*;
+import java.nio.file.StandardOpenOption;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Timer timer = new Timer(0, 0);
-        while (true) {
-            System.out.println(timer);
-            timer.advance();
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+    public static void main(String[] args) {
+        Product tapeMeasure = new Product("Tape measure");
+        Product plaster = new Product("Plaster", "home improvement section");
+        Product tyre = new Product("Tyre", 5);
+
+        System.out.println(tapeMeasure);
+        System.out.println(plaster);
+        System.out.println(tyre);
     }
 }
+    
