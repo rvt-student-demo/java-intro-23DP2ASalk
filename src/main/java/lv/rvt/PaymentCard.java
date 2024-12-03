2f6dabad8bb07bc32642dad48fb205e17c333c2a
23DP2ASalk <167065543+23DP2ASalk@users.noreply.github.com>
2024-12-03 13:42:15 +0000

    Card payments (4 sections)

--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -3,30 +3,15 @@ package lv.rvt;
 public class PaymentCard {
     private double balance;
 
-
     public PaymentCard(double openingBalance){
         this.balance = openingBalance;
     }
-
     public String toString(){
         return "The card has a balance of " + this.balance + " euros";
     }
-
-    public void eatAffordably(){
-        this.balance = this.balance-2.60;
-        if (this.balance < 0){
-            this.balance = this.balance+2.60;
-        }
-        
+    public double balance() {
+        return this.balance;
     }
-
-    public void eatHeartily(){
-        this.balance = this.balance-4.60;
-        if (this.balance < 0){
-            this.balance = this.balance+4.60;
-        }
-    }
-
     public void addMoney(double amount){
         double x = this.balance + amount;
         if (amount < 0){
@@ -37,5 +22,12 @@ public class PaymentCard {
                 this.balance = x;
             }
         } 
+    }
+    public boolean takeMoney(double amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            return true;
+        }
+        return false;
     }
 }
