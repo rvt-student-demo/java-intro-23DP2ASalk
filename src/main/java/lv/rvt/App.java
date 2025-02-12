3d80fba4fd6b8c9f8a0c22d968d56a8dee11c336
23DP2ASalk <167065543+23DP2ASalk@users.noreply.github.com>
2025-02-12 11:41:39 +0000

    Interface In A Box (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,16 +5,16 @@ import java.util.*;
 
 public class App {
     public static void main ( String[] args ) {
-		ArrayList<Person> persons = new ArrayList<Person>();
-		persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
-		persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
+		Box box = new Box(10);
 
-		printPersons(persons);
-    }
-
-	public static void printPersons(ArrayList<Person> persons) {
-		for (Person person : persons) {
-			System.out.println(person);
-		}
+		box.add(new Book("Fyodor Dostoevsky", "Crime and Punishment", 2)) ;
+		box.add(new Book("Robert Martin", "Clean Code", 1));
+		box.add(new Book("Kent Beck", "Test Driven Development", 0.7));
+	
+		box.add(new CD("Pink Floyd", "Dark Side of the Moon", 1973));
+		box.add(new CD("Wigwam", "Nuclear Nightclub", 1975));
+		box.add(new CD("Rendezvous Park", "Closer to Being Here", 2012));
+	
+		System.out.println(box);
 	}
 }
--- src/main/java/lv/rvt/Box.java
+++ src/main/java/lv/rvt/Box.java
@@ -1,58 +1,35 @@
 package lv.rvt;
 
-public class Box {
-    private double width;
-    private double height;
-    private double length;
-    
-    public Box(double width, double height, double length) {
-        this.width = width;
-        this.height = height;
-        this.length = length;
-    }
-    public Box(double side) {
-        this.width = side;
-        this.height = side;
-        this.length = side;
-    }
-    public Box(Box oldBox) {
-        this.width = oldBox.width;
-        this.height = oldBox.height;
-        this.length = oldBox.length;
-    }
-    public double width() {
-        return width;
-    }
-    public double height() {
-        return height;
-    }
-    public double length() {
-        return length;
-    }
-    public double volume() {
-        return width * height * length;
-    }
-    public double area() {
-        return 2 * faceArea() + 2 * topArea() + 2 * sideArea();
-    }
-    private double faceArea() {
-        return length * height;
-    }
-    private double topArea() {
-        return width * length;
-    }
-    private double sideArea() {
-        return height * width;
-    }
-    public Box biggerBox(Box oldBox) {
-        return new Box(1.25 * oldBox.width(), 1.25 * oldBox.height(), 1.25 * oldBox.length());
-    }
-    public Box smallerBox(Box oldBox) {
-        return new Box(0.75 * oldBox.width(), 0.75 * oldBox.height(), 0.75 * oldBox.length());
-    }
-    public boolean nests(Box outsideBox) {
-        return this.width < outsideBox.width &&
-               this.height < outsideBox.height &&
-               this.length < outsideBox.length;
-    }
+import java.util.ArrayList;
+
+public class Box implements Packable {
+	private double capacity;
+	private ArrayList<Packable> items;
+
+	public Box(double capacity) {
+		this.capacity=capacity;
+		items = new ArrayList<>();
+	}
+
+
+	public void add(Packable item) {
+		if (this.capacity >= item.weight()) {
+			items.add(item);
+			this.capacity -= item.weight();
+		}
+	}
+
+	public double weight() {
+		double weight = 0;
+		for (Packable item: items) {
+			weight += item.weight();
+		}
+		return weight;
+	}
+
+	@Override
+	public String toString() {
+		// Box: 6 items, total weight 4.0 kg
+		return "Box: " + items.size() + " items, total weight " + weight() + " kg";
+	}
 }
